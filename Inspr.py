c61ef72e3d64f7ca6e9a51752da1162c38681955
zhix <wzx181@gmail.com>
2016-12-05 14:36:33 +0800

    增加了首选项设置

--- Inspr.py
+++ Inspr.py
@@ -3,8 +3,34 @@ import sublime_plugin
 import urllib
 import json
 
-SETTING_FILE = "Inspr.sublime-settings"
+SETTING_FILE = 'Inspr.sublime-settings'
 
+# Settings
+DICTIONARY_SOURCE   = 'dictionary_source'
+CASE_STYLE          = 'case_style'
+MAXIMUM_QUERY_CHARS = 'maximum_query_characters'
+MAXIMUM_CACHE_WORDS = 'maximum_cache_words'
+CLEAR_SELECTION     = 'clear_selection'
+SKIP_PRONOUN        = 'skip_pronoun'
+FULL_INSPIRATION    = 'full_inspiration'
+ENABLE_CONTEXT_MENU = 'enable_context_menu'
+PROXY               = ''
+
+RANGE_OF_QUERY_CHARS = (1, 32)
+RANGE_OF_CACHE_WORDS = (0, 32768)
+
+# Default Settings Value
+DEFAULT_DIC_SROUCE          = ['Youdao']
+DEFAULT_CASE_STYLE          = 'CamelCase'
+DEFAULT_MAX_QUERY_CHARS     = 32
+DEFAULT_MAX_CACHE_WORDS     = 512
+DEFAULT_CLEAR_SELECTION     = True
+DEFAULT_SKIP_PRONOUN        = True
+DEFAULT_FULL_INSPIRATION    = False
+DEFAULT_ENABLE_CONTEXT_MENU = True
+DEFAULT_PROXY               = ''
+
+# Youdao source
 YOUDAO_API_URL  = 'http://fanyi.youdao.com/openapi.do?'
 YOUDAO_API_ARGS = {
     'key':     '1787962561',
@@ -15,7 +41,8 @@ YOUDAO_API_ARGS = {
     'q':       ''
 }
 
-MAXIMUM_CACHE_SIZE = 512
+# Google source
+# Microsoft source
 
 GLOBAL_CACHE = {}
 
@@ -26,11 +53,19 @@ class InsprReplaceSelectionCommand(sublime_plugin.TextCommand):
         if 'text' not in replacement:
             return
 
+        settings = sublime.load_settings(SETTING_FILE)
         view = self.view
         selection = view.sel()
         translation = replacement['text']
 
         view.replace(edit, selection[0], translation)
+
+        print(settings.get(CLEAR_SELECTION))
+
+        clear_selection = settings.get(CLEAR_SELECTION, DEFAULT_CLEAR_SELECTION)
+        if clear_selection == False:
+            return
+
         pt = selection[0].end()
 
         selection.clear()
@@ -42,6 +77,7 @@ class InsprCommand(sublime_plugin.TextCommand):
 
     def run(self, edit, **args):
 
+        settings = sublime.load_settings(SETTING_FILE)
         view = self.view
         self.available_trans = []
         cache = GLOBAL_CACHE
@@ -97,7 +133,7 @@ class InsprCommand(sublime_plugin.TextCommand):
 
         self.available_trans = sorted(set(self.available_trans))
 
-        if len(cache.keys()) > MAXIMUM_CACHE_SIZE:
+        if len(cache.keys()) > DEFAULT_MAX_CACHE_WORDS:
             cache.clear()
 
         if sel not in cache:
