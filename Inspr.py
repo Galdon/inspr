452af050feed6021df78b207f0694ec6bb9c5f41
zhix <zhix>
2016-12-05 13:41:07 +0800

    增加了 .sublime-settings 文件，重构了主程序逻辑，缓存了已查过的词语

--- Inspr.py
+++ Inspr.py
@@ -4,69 +4,109 @@ import urllib
 import json
 
 SETTING_FILE = "Inspr.sublime-settings"
-YOUDAO_URL   = "http://fanyi.youdao.com/openapi.do?"
 
-KEY = "1787962561"
-KEYFROM = "f2ec-org"
-TYPE = "data"
-DOCTYPE = "json"
-VERSION = "1.1"
+YOUDAO_API_URL  = 'http://fanyi.youdao.com/openapi.do?'
+YOUDAO_API_ARGS = {
+    'key':     '1787962561',
+    'keyfrom': 'f2ec-org',
+    'type':    'data',
+    'doctype': 'json',
+    'version': '1.1',
+    'q':       ''
+}
+
+MAXIMUM_CACHE_SIZE = 512
+
+GLOBAL_CACHE = {}
 
 class InsprReplaceSelectionCommand(sublime_plugin.TextCommand):
+
     def run(self, edit, **replacement):
+
+        if 'text' not in replacement:
+            return
+
         view = self.view
-        view.replace(edit, view.sel()[0], replacement['text'])
+        selection = view.sel()
+        translation = replacement['text']
 
-        pt = view.sel()[0].end()
+        view.replace(edit, selection[0], translation)
+        pt = selection[0].end()
 
-        view.sel().clear()
-        view.sel().add(sublime.Region(pt))
+        selection.clear()
+        selection.add(sublime.Region(pt))
 
         view.show(pt)
 
 class InsprCommand(sublime_plugin.TextCommand):
+
     def run(self, edit, **args):
 
-        sel = self.view.substr(self.view.sel()[0])
+        view = self.view
+        self.available_trans = []
+        cache = GLOBAL_CACHE
+
+        sel = view.substr(view.sel()[0])
         if sel == '':
             return
 
-        query = {
-            'key': KEY,
-            'keyfrom': KEYFROM,
-            'type': TYPE,
-            'doctype': DOCTYPE,
-            'version': VERSION,
-            'q': sel
-        }
-        response = urllib.request.urlopen(YOUDAO_URL + urllib.parse.urlencode(query))
+        # if cache hit
+        if sel in cache:
+            cache_styles = cache[sel]
+            code_style = args['camel_case_type']
+            if code_style in cache_styles:
+                cache_trans = cache_styles[code_style]
+                self.available_trans = cache_trans
+                view.window().show_quick_panel(self.available_trans, self.on_done)
+                return
+
+        YOUDAO_API_ARGS['q'] = sel
+
+        url = YOUDAO_API_URL + urllib.parse.urlencode(YOUDAO_API_ARGS)
+        response = urllib.request.urlopen(url)
 
         data = response.read()
         encoding = response.info().get_content_charset('utf-8')
         result = json.loads(data.decode(encoding))
 
+        if 'errorCode' in result:
+            if result['errorCode'] != 0:
+                return
 
-        self.available_trans = []
+        candidates = []
+
         if 'translation' in result:
-            for x in result['translation']:
-                case = ""
-                if args['camel_case_type'] == 'upper':
-                    case = self.upper_camel_case(x)
-                else:
-                    case = self.lower_camel_case(x)
-                self.available_trans.append(case)
+            for v in result['translation']:
+                candidates.append(v)
         if 'web' in result:
             for web in result['web']:
                 if web['key'] == sel:
-                    case = ""
-                    for x in web['value']:
-                        if args['camel_case_type'] == 'upper':
-                            case = self.upper_camel_case(x)
-                        else:
-                            case = self.lower_camel_case(x)
-                        self.available_trans.append(case)
-        self.available_trans = sorted(set(self.available_trans), key=self.available_trans.index)
-        self.view.window().show_quick_panel(self.available_trans, self.on_done)
+                    value = web['value']
+                    for v in value:
+                        candidates.append(v)
+
+        case_style = args['camel_case_type']
+
+        for trans in candidates:
+            case = ''
+            if case_style == 'upper':
+                case = self.upper_camel_case(trans)
+            else:
+                case = self.lower_camel_case(trans)
+            self.available_trans.append(case)
+
+        self.available_trans = sorted(set(self.available_trans))
+
+        if len(cache.keys()) > MAXIMUM_CACHE_SIZE:
+            cache.clear()
+
+        if sel not in cache:
+            cache[sel] = {}
+        if case_style not in cache[sel]:
+            cache[sel][case_style] = []
+        cache[sel][case_style] = self.available_trans
+
+        view.window().show_quick_panel(self.available_trans, self.on_done)
 
     def upper_camel_case(self, x):
         s = ''.join(a for a in x.title() if not a.isspace())
